d0fc3d21577fe191704dec80084ca9d42e181dc2
Joydeep-Pal <joydeeppal2005@gmail.com>
2025-04-01 07:22:23 +0530

    commit

--- 18.3 constructor function and object/index.js
+++ 18.3 constructor function and object/index.js
@@ -1,11 +1,17 @@
 
-function Guest(Name,Age){
+function Guest(Name,Age,languageKnown){
     this.Name = Name;
     this.Age = Age;
+    this.languageKnown = languageKnown;
+    this.sayHello = function (){
+        console.log("Hello");
+    }
 }
 
-var guest1 = new Guest("ABC",19);
-var guest2 = new Guest("DEF",22);
+var guest1 = new Guest("ABC",19,["bengali","english"]);
+var guest2 = new Guest("DEF",22,["bengali","english","japanese"]);
 
 console.log(guest1.Name); // ABC
 console.log(guest1.Age);  // 19
+console.log(guest1.languageKnown);  //["bengali","english"]
+console.log(guest1.sayHello());  // Hello
