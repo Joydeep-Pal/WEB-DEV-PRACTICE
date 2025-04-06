d3e586945405b762d1bb54e447c9fb43cb342b1d
Joydeep-Pal <joydeeppal2005@gmail.com>
2025-04-06 12:33:46 +0530

    commit

---
+++ jQuery/19.2 attributes/index.js
@@ -0,0 +1,13 @@
+
+// SetAttributes 
+
+// $("<selector>").attr("<attribute>" , "<value>");
+
+
+// GetAttributes
+
+// $("<selector>").attr("<attribute>");  -> returns the vaue of the attribute
+
+
+
+$("a").attr("href","https://www.yahoo.com");
