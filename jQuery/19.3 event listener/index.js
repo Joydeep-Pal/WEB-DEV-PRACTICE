aa38a3e375650448100f8eb92fb4686e69e61dcb
Joydeep-Pal <joydeeppal2005@gmail.com>
2025-04-06 17:03:01 +0530

    commit

---
+++ jQuery/19.3 event listener/index.js
@@ -0,0 +1,12 @@
+
+// Adding Event Listener
+
+// $("<selector>").<event_type>(function(){
+//      ------------------------
+// });
+
+
+$("h1").click(function(){
+    $("h1").css("background-color","orange");
+    $("h1").css("color","purple");
+});
