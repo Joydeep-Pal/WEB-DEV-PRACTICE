cae3fc407f72245764387dd5f845291bae16592b
Joydeep-Pal <joydeeppal2005@gmail.com>
2025-04-07 21:55:52 +0530

    commit

--- jQuery/19.3 event listener/index.js
+++ jQuery/19.3 event listener/index.js
@@ -10,3 +10,11 @@ $("h1").click(function(){
     $("h1").css("background-color","orange");
     $("h1").css("color","purple");
 });
+
+
+$(document).keydown(function(){
+    setTimeout(function(){
+        $("h1").css("background-color","black");
+        $("h1").css("color","white");
+    },100);
+});
