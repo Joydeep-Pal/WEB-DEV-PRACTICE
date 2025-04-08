56417f2be8eea742946a08ef96f9f024ce735c8f
Joydeep-Pal <joydeeppal2005@gmail.com>
2025-04-08 12:59:09 +0530

    commit

---
+++ jQuery/19.4 add, del elements/index.js
@@ -0,0 +1,13 @@
+
+//  Adding html elements
+
+//$("<selector>").before("<html_element>");     --> adds before the selected element
+//$("<selector>").after("<html_element>");      --> adds after the selected element
+//$("<selector>").prepend("<html_element>");    --> adds before the contents of the selected element
+//$("<selector>").append("<html_element>");     --> adds after the contents of the selected element
+
+
+$("h1").before("<button>Before</button>");
+$("h1").after("<button>After</button>");
+$("h1").prepend("<button>Prepend</button>");
+$("h1").append("<button>Append</button>");
