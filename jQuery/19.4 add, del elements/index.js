df22205faf981a717f7cc6d6956a07824e553878
Joydeep-Pal <joydeeppal2005@gmail.com>
2025-04-09 08:09:32 +0530

    commit

--- jQuery/19.4 add, del elements/index.js
+++ jQuery/19.4 add, del elements/index.js
@@ -11,3 +11,12 @@ $("h1").before("<button>Before</button>");
 $("h1").after("<button>After</button>");
 $("h1").prepend("<button>Prepend</button>");
 $("h1").append("<button>Append</button>");
+
+
+
+//  Removing html element
+
+//$("<selector>").remove();    --> removes the selected element
+
+
+$("h1").remove();
