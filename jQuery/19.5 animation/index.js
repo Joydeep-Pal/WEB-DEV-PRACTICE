9c41fcbfadba1f3a431d383a36ee7706b2a41aaa
Joydeep-Pal <joydeeppal2005@gmail.com>
2025-04-12 17:48:21 +0530

    commiit

--- jQuery/19.5 animation/index.js
+++ jQuery/19.5 animation/index.js
@@ -20,3 +20,25 @@ $(".hide").click(function(){
 $(".fade").click(function(){
     $("h1").fadeToggle();
 })
+
+
+
+// Slide element
+
+// $("<selector>").slideUp();      --> slide element up
+// $("<selector>").slideDown();    --> slide element down
+
+$(".slide").click(function(){
+    $("h1").slideToggle();
+})
+
+
+
+// Animate element
+
+// this help to change css of the selected element. But we can only use those properties that have value
+// $("<selector>").animate({property : value});
+
+$(".animate").click(function(){
+    $("h1").animate({opacity:0.5});
+})
